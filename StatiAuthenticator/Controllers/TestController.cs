65b47bb3a50364cf48ff68017c0922f02667cff3
mohamed gueliane <mohamed.gueliane@live.com>
2024-05-06 02:56:06 +0200

    Ajoutez des fichiers projet.

---
+++ StatiAuthenticator/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StatiAuthenticator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Redirect("portwin:/callback");
+        }
+    }
+}
