250c07c7527c17b37c5d9610d8585f7d1f9c880c
mohamed gueliane <mohamed.gueliane@live.com>
2024-05-06 03:31:42 +0200

    xxx

---
+++ StatiAuthenticator/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StatiAuthenticator.Controllers
+{
+    //rout to index 
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // return empty page with text 
+            return Content("Hello, World!");
+
+        }
+    }
+}
